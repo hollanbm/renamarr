f6896ce91fc3490e049c5ab89322ce215a0e18fd
Brad Holland <hollanbm@gmail.com>
2024-04-25 05:12:30 +0000

    add error handling and logging

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # sonarr-series-scanner
+
+## Requirements
+
+This script is intended to be used alongside the `Episode Title Required` setting
+
+* Settings
+  * Media Management
+    * Episode Title Required
+      * Always
+
+### The problem
+
+It is fairly common for the TVDB to be updated the day of, or even after airing. Sonarr refreshes it's TVDB cache every 24 hours.
+
+Unfortunately, in extreme circumstances, this can prevent import for up to 24 hours.
+
+To solve this, I created this app
+
+### How it works
+
+This app uses the [Sonarr API](https://sonarr.tv/docs/api/) to do the following
+
+* Iterate over continuing [series](https://sonarr.tv/docs/api/#/Series/get_api_v3_series)
+  * If a series has grabbed episode, with TBA title
+    * will trigger a series refresh, to hopefully pull new info from The TVDB
+
+### Usage
+
+The app will immediately exit upon completion. You will need to use cron or similar, to handle scheduling. Recomended interval is 1 hour.
--- src/main.py
+++ src/main.py
@@ -1,20 +1,53 @@
-from pycliarr.api import SonarrCli
-import os
+import os, sys
+from pycliarr.api import SonarrCli,CliServerError
+from loguru import logger
+#from dotenv import load_dotenv
 
-from dotenv import load_dotenv
-load_dotenv()
+#load_dotenv()
 
 def series_scanner():
   sonarr_cli = SonarrCli(os.environ["SONARR_URL"], os.environ["SONARR_API_KEY"])
 
   series = sonarr_cli.get_serie()
 
-  for show in series:
-    if (show.status.lower() == "continuing" and 
-        grabbed_episodes_without_name(sonarr_cli.get_episode(show.id))):
-      sonarr_cli.refresh_serie(show.id)
+  if series is []:
+    logger.error("Sonarr returned empty series list")
+    exit(0)
+  else:
+    logger.debug("Retrieved series list")
 
-def grabbed_episodes_without_name(episode_list):
-  # Filter episode list, so it only contains episodes with an airDate, and no Title
-  episode_list[:] = [d for d in episode_list if d.get('title') == 'TBA' and d.get('grabbed')]
-  return len(episode_list) > 0
+  for show in sorted(series, key=lambda s: s.title):
+    if show.status.lower() == "continuing" :
+      episode_list = sonarr_cli.get_episode(show.id)
+      
+      if episode_list is []:
+        logger.error(f'{show.title} - Error fetching episode list')
+        exit(1)
+      else:
+        logger.debug(f'{show.title} - Retrieved episode list')
+
+      # Filter episode list, so it only contains grabbed episodes, with TBA title
+      episode_list[:] = [e for e in episode_list if e.get('title') == 'TBA' and e.get('grabbed')]
+
+      if len(episode_list) > 0:
+        logger.info(f'{show.title} - Found grabbed episode with TBA title')
+        logger.info(f'{show.title} - Rescanning series')
+
+        sonarr_cli.refresh_serie(show.id)
+      else:
+        logger.debug(f'{show.title} - No grabbed episodes with TBA title')
+
+if __name__ == '__main__':
+  if os.environ.get('SONARR_URL') is None:
+    logger.error("SONARR_URL not set")
+    exit(1)
+
+  if os.environ.get('SONARR_API_KEY') is None:
+    logger.error("SONARR_API_KEY not set")
+    exit(1)
+
+  try:
+    series_scanner()
+  except CliServerError as exc:
+    logger.error(exc)
+    exit(1)
