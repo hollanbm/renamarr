89856ca3d7ae07355a39aabc1694b0880f826920
Brad Holland <hollanbm@gmail.com>
2024-04-28 02:57:57 +0000

    setup postCreateCommand.sh

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# create venv
+poetry install --no-interaction --no-ansi --quiet
+
+# activate venv
+source $(poetry env info --path)/bin/activate
+
+# setup pre-commit hook
+pre-commit install
